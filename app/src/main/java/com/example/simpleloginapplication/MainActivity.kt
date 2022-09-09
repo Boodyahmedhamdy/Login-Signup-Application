c84c01fea4bcdf3eaaf4f08002df4dc7fb10bd54
unknown <abdelrahmanahmedhamdy99@gmail.com>
2022-09-09 09:17:07 +0200

    setup the action bar with back button

--- app/src/main/java/com/example/simpleloginapplication/MainActivity.kt
+++ app/src/main/java/com/example/simpleloginapplication/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.simpleloginapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.simpleloginapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // hold the navigation controller
+        navController = findNavController(R.id.mainFragmentContainer)
+        // setup the action bar -- top left back button
+        setupActionBarWithNavController(navController)
+
+    }
+
+    // this method must be overridden when setting up action bar with navController
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
